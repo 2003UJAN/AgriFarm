d0a0bc1bd6307d79d6a0a6c6da606c9b679297c6
Ujan Pradhan <113442287+2003UJAN@users.noreply.github.com>
2023-04-21 10:18:10 +0530

    Update CategoryApi.js

--- src/componentApi/CategoryApi.js
+++ src/componentApi/CategoryApi.js
@@ -10,5 +10,42 @@ export const ApiCategories =[
     {
         title: 'Cauliflower',
         src : 'https://www.pexels.com/photo/close-up-shot-of-a-cauliflower-4162149/'
+    },
+    {
+        title: 'Garlic',
+        src : 'https://www.pexels.com/photo/garlic-bulbs-on-brown-surface-1392585/',
+    },
+
+    {
+        title: 'Chilly',
+        src : 'https://www.pexels.com/photo/photo-of-red-chillies-39390/',
+    },
+    {
+        title: 'Beans',
+        src : 'https://www.pexels.com/photo/close-up-photo-of-raw-green-beans-3004798/',
+    },
+    {
+        title: 'Beans',
+        src : 'https://www.pexels.com/photo/close-up-photo-of-raw-green-beans-3004798/',
+    },
+    {
+        title: 'Beans',
+        src : 'https://www.pexels.com/photo/close-up-photo-of-raw-green-beans-3004798/',
+    },
+    {
+        title: 'Ladys Finger',
+        src : 'https://www.pexels.com/photo/ladies-fingers-lot-2583187/',
+    },
+    {
+        title: 'Brinjal',
+        src : 'https://www.pexels.com/photo/crop-ethnic-farmer-with-eggplants-in-box-on-plantation-5529949/',
+    }
+    {
+        title: 'Raddish',
+        src : 'https://www.pexels.com/photo/person-taking-raddish-from-a-store-shelf-8805437/',
+    }
+    {
+        title: 'Cucumber',
+        src : 'https://www.pexels.com/photo/cucumber-2329440/',
     }
 ]
